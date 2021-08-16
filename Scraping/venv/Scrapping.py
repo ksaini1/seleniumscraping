58590462f9cfa10e7b4b59b82a291c184bffe64b
ksaini1 <47371717+ksaini1@users.noreply.github.com>
2021-08-15 23:32:29 -0700

    All elements

--- Scraping/venv/Scrapping.py
+++ Scraping/venv/Scrapping.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 from lxml import html
+from selenium.common.exceptions import NoSuchElementException
+import csv
+
+
 options = webdriver.ChromeOptions()
 options.headless = True
 # options.add_argument()
@@ -44,42 +48,45 @@ accept_button.click()
 # print(fin)
 a = 3
 # OPENING EACH SEARCH RECORDS
-while a < 43:
-    det = driver.find_element_by_id("datagrid_results__ctl" + str(a) + "_name").get_attribute("href")
-    new = driver.get(det)
-    driver.implicitly_wait(10)
-    driver.get_screenshot_as_file("filename.png")
+with open("searchres.csv",'w') as csvfile:
+    fieldnames = ['First Name','Middle Name','Last Name','License Type','License Number','Status','Issue Date','Expiry Date']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    while a < 43:
+        det = driver.find_element_by_id("datagrid_results__ctl" + str(a) + "_name").get_attribute("href")
+        new = driver.get(det)
+        driver.implicitly_wait(10)
+        driver.get_screenshot_as_file("filename.png")
 
     # fname = driver.find_element_by_id("_ctl27__ctl1_first_name")
-    fname = driver.find_element_by_xpath("//*[contains(@id, '_first_name')]")
-    middle= driver.find_element_by_xpath("//*[contains(@id, '_m_name')]")
-    lname = driver.find_element_by_xpath("//*[contains(@id, 'last_name')]")
-    lic = driver.find_element_by_xpath("//*[contains(@id, 'license_no')]")
-    lictype = driver.find_element_by_xpath("//*[contains(@id, '_license_type')]")
-    status = driver.find_element_by_xpath("//*[contains(@id, '_status') and @maxlength='50']")
-    issuedate = driver.find_element_by_xpath("//*[contains(@id, '_issue_date') and @maxlength='50']")
-    expdate = driver.find_element_by_xpath("//*[contains(@id, '_expiry') and @maxlength='50']")
+    # fname = driver.find_element(By.XPATH("//*[contains(@id, '_first_name')]"))
+    # fname1 =driver.find_element(By.XPATH("//*[contains(@id, '_first_name')]"))
+        try:
+            fname = driver.find_element_by_xpath("//*[contains(@id, '_first_name')]")
+            middle= driver.find_element_by_xpath("//*[contains(@id, '_m_name')]")
+            lname = driver.find_element_by_xpath("//*[contains(@id, 'last_name')]")
+            lic = driver.find_element_by_xpath("//*[contains(@id, 'license_no')]")
+            lictype = driver.find_element_by_xpath("//*[contains(@id, '_license_type')]")
+            status = driver.find_element_by_xpath("//*[contains(@id, '_status') and @maxlength='50']")
+            issuedate = driver.find_element_by_xpath("//*[contains(@id, '_issue_date') and @maxlength='50']")
+            expdate = driver.find_element_by_xpath("//*[contains(@id, '_expiry') and @maxlength='50']")
 
-    print(fname.text)
-    print(middle.text)
-    print(lname.text)
-    print(lic.text)
-    print(lictype.text)
-    print(status.text)
-    print(issuedate.text)
-    print(expdate.text)
-    print(" ")
-    del_but = driver.find_element_by_id("btn_close")
-    del_but.click()
+    # print(fname1)
+            writer.writerow({'First Name': fname.text, 'Middle Name': middle.text, 'Last Name': lname.text, 'License Type': lictype.text, 'License Number': lic.text, 'Status':status.text, 'Issue Date': issuedate.text, 'Expiry Date': expdate.text})
+            print(fname.text)
+            print(" ")
+            del_but = driver.find_element_by_id("btn_close")
+            del_but.click()
+        except NoSuchElementException:
+            pass
     # OPEN THE ORIGINAL PAGE AGAIN
-    driver.get('https://idbop.mylicense.com/verification/')
-    driver.implicitly_wait(10)
-    inp = driver.find_element_by_id("t_web_lookup__last_name")
-    inp.send_keys('L')
-    accept_button = driver.find_element_by_id("sch_button")
-    accept_button.click()
-    driver.implicitly_wait(10)
-    a+=1
+        driver.get('https://idbop.mylicense.com/verification/')
+        driver.implicitly_wait(10)
+        inp = driver.find_element_by_id("t_web_lookup__last_name")
+        inp.send_keys('L')
+        accept_button = driver.find_element_by_id("sch_button")
+        accept_button.click()
+    # driver.implicitly_wait(10)
+        a+=1
 
 
-# driver.close()
